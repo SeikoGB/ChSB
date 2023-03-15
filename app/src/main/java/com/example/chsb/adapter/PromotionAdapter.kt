fefb8f77e1cee56c9abcf5b536467c6246dd86a6
sqljava <sqljava456@gmail.com>
2023-03-15 10:58:11 +0500

    q

---
+++ app/src/main/java/com/example/chsb/adapter/PromotionAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.chsb.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PromotionAdapter(): RecyclerView.Adapter<PromotionAdapter.PromotionHolder>() {
+
+    class PromotionHolder(view:View): RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PromotionHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PromotionHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/chsb/model/Promotion.kt
@@ -0,0 +1,4 @@
+package com.example.chsb.model
+
+class Promotion {
+}
