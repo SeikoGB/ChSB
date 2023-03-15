ca796a906463c566f2702574361e67b9a61d528f
PC670 <ismoilovashahina@gmail.com>
2023-03-15 11:00:05 +0500

    re4gistrato

--- app/src/main/java/com/example/chsb/SignUpActivity.kt
+++ app/src/main/java/com/example/chsb/SignUpActivity.kt
@@ -3,10 +3,13 @@ package com.example.chsb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class RegistrationActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+        setContentView(R.layout.activity_sign_up)
+
+
+        var sharedPreferences = getSharedPreferences("user", MODE_PRIVATE)
 
 
     }
