a6f2db495d283671ee28de0bd5b2b75d93dbac4e
PC670 <ismoilovashahina@gmail.com>
2023-03-15 11:33:40 +0500

    registr

--- app/src/main/java/com/example/chsb/RegistrationActivity.kt
+++ app/src/main/java/com/example/chsb/RegistrationActivity.kt
@@ -1,21 +1,39 @@
 package com.example.chsb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.chsb.databinding.ActivityRegistrationBinding
 import com.example.chsb.model.User
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import kotlin.math.log
 
 class RegistrationActivity : AppCompatActivity() {
+    lateinit var binding: ActivityRegistrationBinding
+    lateinit var users_data : MutableList<User>
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration)
+        setContentView(binding.root)
 
         var type = object : TypeToken<List<User>>(){}.type
         var gson = Gson()
         var sharedPreferences = getSharedPreferences("user", MODE_PRIVATE)
         var edit = sharedPreferences.edit()
         var datas = sharedPreferences.getString("users","")
+
+        binding.loginButton.setOnClickListener {
+            if (datas == ""){
+                users_data = mutableListOf()
+                Toast.makeText(this, "you have not saved changes", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                users_data = gson.fromJson(datas,type)
+            }
+        }
+
 
 
 
--- app/src/main/java/com/example/chsb/SignUpActivity.kt
+++ app/src/main/java/com/example/chsb/SignUpActivity.kt
@@ -63,5 +63,7 @@ class SignUpActivity : AppCompatActivity() {
 
 
 
+
+
     }
 }
