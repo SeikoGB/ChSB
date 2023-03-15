098399f60e400966dcbe999d57693f2558050de4
behruzbek2Gulomov <120156394+behruzbek2Gulomov@users.noreply.github.com>
2023-03-15 11:23:33 +0500

    bbb

--- app/src/main/java/com/example/chsb/MenuActivity.kt
+++ app/src/main/java/com/example/chsb/MenuActivity.kt
@@ -1,11 +1,28 @@
 package com.example.chsb
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.chsb.databinding.ActivityMenuBinding
+import com.example.chsb.model.CourseAdapter
 
 class MenuActivity : AppCompatActivity() {
+
+    var popular_list= arrayListOf<Course>()
+    var all_couses= arrayListOf<Course>()
+    var newest= arrayListOf<Course>()
+    var binder= ActivityMenuBinding.inflate(layoutInflater)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        setContentView(binder.root)
+
+
+
+        var popular = CourseAdapter(popular_list)
+        var popularman = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
+
+
     }
 }
